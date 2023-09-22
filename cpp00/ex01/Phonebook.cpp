a8e2410fce988f3fc0b6cf8368e328f2b70b446c
Talhadmr <demirtalha093@gmail.com>
2023-09-22 20:17:39 +0300

    index

--- cpp00/ex01/Phonebook.cpp
+++ cpp00/ex01/Phonebook.cpp
@@ -12,56 +12,56 @@ void Phonebook::add_to_book(void)
     
     cout << "Name: ";
     getline(cin, val);
-    if(val.length() < 12)
+    if(val.length() < 11)
         this->contacts[this->cont_num % 8].setname(val);
-    else if(val.length() > 11)
+    else if(val.length() > 10)
     {
-        val[10] = '.';
-        this->contacts[this->cont_num % 8].setname(val.substr(0,11));
+        val[9] = '.';
+        this->contacts[this->cont_num % 8].setname(val.substr(0,10));
     }
     val.clear();
 
     cout << "surname: ";
     getline(cin, val);
-    if(val.length() < 12)
+    if(val.length() < 11)
      this->contacts[this->cont_num % 8].setsurname(val);
-    else if(val.length() > 11)
+    else if(val.length() > 10)
     {
         val[10] = '.';
-        this->contacts[this->cont_num % 8].setsurname(val.substr(0,11));
+        this->contacts[this->cont_num % 8].setsurname(val.substr(0,10));
     }
     val.clear();
 
     cout << "nickname: ";
     getline(cin, val);
-    if(val.length() < 12)
+    if(val.length() < 11)
         this->contacts[this->cont_num % 8].setnickname(val);
-    else if(val.length() > 11)
+    else if(val.length() > 10)
     {
-        val[10] = '.';
-        this->contacts[this->cont_num % 8].setnickname(val.substr(0,11));
+        val[9] = '.';
+        this->contacts[this->cont_num % 8].setnickname(val.substr(0,10));
     }
     val.clear();
 
     cout << "phone number: ";
     getline(cin, val);
-    if(val.length() < 12)
+    if(val.length() < 11)
         this->contacts[this->cont_num % 8].setphone_number(val);
-    else if(val.length() > 11)
+    else if(val.length() > 10)
     {
-        val[10] = '.';
-        this->contacts[this->cont_num % 8].setphone_number(val.substr(0,11));
+        val[9] = '.';
+        this->contacts[this->cont_num % 8].setphone_number(val.substr(0,10));
     }
     val.clear();
 
     cout << "darkest secret: ";
     getline(cin, val);
-    if(val.length() < 12)
+    if(val.length() < 11)
         this->contacts[this->cont_num % 8].setdarkest_secret(val);
-    else if(val.length() > 11)
+    else if(val.length() > 10)
     {
         val[10] = '.';
-        this->contacts[this->cont_num % 8].setdarkest_secret(val.substr(0,11));
+        this->contacts[this->cont_num % 8].setdarkest_secret(val.substr(0,10));
     }
     val.clear();
     this->cont_num++;
@@ -73,12 +73,13 @@ int Phonebook::table()
     int j;
 
     i = 0;
-    cout << "Name       | Surname   | Nickname  |Phone numb |dark secret|"<< std::endl;
-    cout << "------------------------------------------------------------"<< std::endl;
+    cout << "index     |Name      | Surname  | Nickname |"<< std::endl;
+    cout << "--------------------------------------------"<< std::endl;
     while (i < this->cont_num && i < 8 )
     {
+        cout<<"         " << i << "|";
         j = 0;
-        len = 11 - this->contacts[i].getname().length();
+        len = 10 - this->contacts[i].getname().length();
         while(j < len)
         {
             cout << " ";
@@ -87,7 +88,7 @@ int Phonebook::table()
         cout <<this->contacts[i].getname() << "|";
 
         j = 0;
-        len = 11 - this->contacts[i].getsurname().length();
+        len = 10 - this->contacts[i].getsurname().length();
         while(j < len)
         {
             cout << " ";
@@ -97,33 +98,15 @@ int Phonebook::table()
 
 
         j = 0;
-        len = 11 - this->contacts[i].getnickname().length();
+        len = 10 - this->contacts[i].getnickname().length();
         while(j < len)
         {
             cout << " ";
             j++;
         }
-        cout <<this->contacts[i].getnickname() << "|";
-
-
-        j = 0;
-        len = 11 - this->contacts[i].getphone_number().length();
-        while(j < len)
-        {
-            cout << " ";
-            j++;
-        }
-        cout <<this->contacts[i].getphone_number() << "|";
-
-
-        j = 0;
-        len = 11 - this->contacts[i].getdarkest_secret().length();
-        while(j < len)
-        {
-            cout << " ";
-            j++;
-        }
-        cout <<this->contacts[i].getdarkest_secret() << "|"<< std::endl;
+        cout <<this->contacts[i].getnickname() << "|" << std::endl;
+        cout << "********************************************"<< std::endl;
+                 
         i++;
     }
     return i;
